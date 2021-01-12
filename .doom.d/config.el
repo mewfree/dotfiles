49e67c3ff111728dcc4b9e3ef0d11b6a4e2db067
Damien Gonot <damien.gonot@gmail.com>
2021-01-12 12:15:30 -0500

    Doom Emacs config change 2020-01-12

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,11 +1,26 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq
- user-full-name "Damien Gonot"
- user-mail-address "damien.gonot@gmail.com"
- doom-theme 'doom-one
- org-directory "~/meworg/"
- display-line-numbers-type 'relative
- projectile-project-search '("~/dev/"))
+(setq user-full-name "Damien Gonot"
+      user-mail-address "damien.gonot@gmail.com"
+      doom-theme 'doom-one
+      display-line-numbers-type 'relative
+      projectile-project-search '("~/dev/"))
+
+(use-package! org-fancy-priorities
+              :hook (org-mode . org-fancy-priorities-mode)
+              :config (setq org-fancy-priorities-list '((?A . "HIGH")
+                                                        (?B . "MEDIUM")
+                                                        (?C . "LOW"))))
+
+(after! org
+        (setq org-directory "~/meworg/"
+              org-agenda-files '("~/meworg/")
+              org-todo-keywords '((sequence "TODO(t)" "PROGRESS(p)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)"))
+              org-todo-keyword-faces '(("TODO" . "turquoise2")
+                                       ("PROGRESS" . "khaki2")
+                                       ("WAITING" . "grey42")
+                                       ("DONE" . "grey42")
+                                       ("CANCELLED" . "grey42")
+                                       )))
 
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
