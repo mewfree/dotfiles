4b1d26bd3bdd300c9887ae56f411720b4a0df138
Damien Gonot <damien.gonot@gmail.com>
2021-10-23 19:25:00 -0400

    larger fonts because I'm getting old

--- lemonbar/lemonbar.sh
+++ lemonbar/lemonbar.sh
@@ -1,1 +1,1 @@
-bash ~/lemonbar_config.sh | lemonbar -p -F#FFFFFFFF -B#FF222222 -f "DejaVu Sans Mono"-10 -f FontAwesome-10
+bash ~/lemonbar_config.sh | lemonbar -p -F#FFFFFFFF -B#FF222222 -f "DejaVu Sans Mono"-11 -f FontAwesome-11
