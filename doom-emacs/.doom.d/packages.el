14c469031a9c01c2becb74cc997e9402d53bb928
Damien Gonot <damien.gonot@gmail.com>
2022-01-02 17:48:23 -0500

    fsharp config

--- doom-emacs/.doom.d/packages.el
+++ doom-emacs/.doom.d/packages.el
@@ -5,6 +5,7 @@
 (package! org-fancy-priorities)
 (package! ob-julia-vterm)
 (package! ob-elixir)
+(package! ob-fsharp)
 (package! gleam-mode
   :recipe (:host github :repo "gleam-lang/gleam-mode"))
 (package! org-fragtog)
