725a5b94d70e27ccb6dc55e7350698098779cf0c
Damien Gonot <damien.gonot@gmail.com>
2022-01-18 20:02:07 -0500

    add ob-zig

--- doom-emacs/.doom.d/packages.el
+++ doom-emacs/.doom.d/packages.el
@@ -7,6 +7,8 @@
 (package! ob-elixir)
 (package! ob-fsharp)
 (package! ob-typescript)
+(package! ob-zig
+  :recipe (:host github :repo "jolby/ob-zig.el"))
 (package! gleam-mode
   :recipe (:host github :repo "gleam-lang/gleam-mode"))
 (package! org-fragtog)
