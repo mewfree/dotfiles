3eb4960f399377449a8635c9ef0ac1024d505ebd
Damien Gonot <dgonot@wealthsimple.com>
2022-06-01 15:02:11 -0400

    add kotlin support to Doom Emacs

--- doom-emacs/.doom.d/init.el
+++ doom-emacs/.doom.d/init.el
@@ -137,7 +137,7 @@
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        (julia +lsp)             ; a better, faster MATLAB
-       ;;kotlin            ; a better, slicker Java(Script)
+       kotlin            ; a better, slicker Java(Script)
        latex             ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
