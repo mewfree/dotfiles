9ecb23eff955309bedf498f663b7da379e5527eb
Damien Gonot <damien.gonot@gmail.com>
2024-11-05 21:12:53 -0500

    add copilot-chat

--- doom-emacs/.doom.d/packages.el
+++ doom-emacs/.doom.d/packages.el
@@ -27,3 +27,5 @@
 (package! literate-calc-mode)
 (package! copilot
   :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
+(package! copilot-chat
+  :recipe (:host github :repo "chep/copilot-chat.el" :files ("*.el")))
